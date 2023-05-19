68978a8f19ae58e8aefd974c395f15a55c6bae41
Funtic-from-Belarus <maksim.kuzyuk@gmail.com>
2023-05-19 20:14:20 +0300

    Slider
    added the response slider. Fixing the white line

---
+++ js/main.js
@@ -0,0 +1,104 @@
+const popularCard = document.querySelectorAll('.popular__cards .popular__card');
+const sliderLine = document.querySelector('.slider-line');
+const popularCards = document.querySelector('.popular__cards')
+let width;
+let count = 0;
+
+
+
+function init() {
+   let widthW = window.innerWidth;
+   console.log('resize');
+   // if (window.innerWidth < 1000) { //надо было прибавить 120 
+   //    popularCards.removeProperty('gap');
+   //    // popularCards.style.gap = 0 + 'px';
+   // } else {
+   //    popularCards.style.gap = 20 + 'px';
+   // }
+   width = document.querySelector('.slider').offsetWidth;
+   sliderLine.style.width = width * popularCard.length + "px";
+
+   if (window.innerWidth < 540) {
+      popularCard.forEach(item => {
+         item.style.width = width + 'px';
+         item.style.height = 'auto';
+      });
+   }
+   else if (window.innerWidth < 920) {
+      popularCard.forEach(item => {
+         item.style.width = (width / 2) + 'px';
+         item.style.height = 'auto';
+      });
+   } else {
+      popularCard.forEach(item => {
+
+         item.style.width = (width / 3) - 20 + 'px';
+         item.style.height = 'auto';
+      });
+   }
+   console.log(widthW);
+
+   rollSlider();
+}
+window.addEventListener('resize', init);
+init();
+console.log(popularCard.length);
+let widthW = window.innerWidth;
+
+document.querySelector('.next').addEventListener('click', () => {
+   count++;
+   console.log(count);
+   if (count >= (popularCard.length - 2) && widthW > 1000) {
+      console.log(window.innerWidth)
+      count = 0;
+      rollSlider();
+   } else if (count >= popularCard.length && widthW < 540) {
+      count = 0;
+      rollSlider();
+   } else if (count >= popularCard.length - 1 && widthW > 540 && window.innerWidth < 940) {
+      count = 0;
+      rollSlider();
+   } else {
+      rollSlider();
+   }
+
+});
+document.querySelector('.back').addEventListener('click', () => {
+   count--;
+   console.log(count);
+   if (count < 0 && widthW < 540) {
+      count = popularCard.length - 1;
+   } else if (count < 0 && widthW > 540 && widthW < 1000) {
+      count = popularCard.length - 2;
+   } else if (count < 0 && widthW > 1000) {
+      count = popularCard.length - 3;
+   }
+   console.log(count);
+   rollSlider();
+   // if (count >= 4 && widthW > 1000) {
+   //    console.log(window.innerWidth)
+   //    count = 0;
+   //    rollSlider();
+   // } else if (count >= 5 && window.innerWidth < 940) {
+   //    count = 0;
+   //    rollSlider();
+   // } else if (count >= 6 && widthW < 540) {
+   //    count = 0;
+   //    rollSlider();
+   // } else {
+   //    rollSlider();
+   // }
+});
+
+function rollSlider() {
+   if (window.innerWidth < 540) {
+      sliderLine.style.transform = 'translate(-' + ((count * width) + (count * 20)) + 'px)';
+
+   }
+   else if (window.innerWidth < 920) {
+      sliderLine.style.transform = 'translate(-' + (count * (width / 2) + count * 20) + 'px)';
+   } else {
+      sliderLine.style.transform = 'translate(-' + (count * (width / 3) - 15) + 'px)';
+   }
+
+}
